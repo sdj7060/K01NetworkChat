753cacdcd753b2f2ad74658633d98b50010823a9
sdj7060 <sdj7060@nate.com>
2020-11-04 17:56:37 +0900

    K01NetworkChat 첫번째

---
+++ src/chat5/Receiver.java
@@ -0,0 +1,47 @@
+package chat5;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.SocketException;
+
+public class Receiver extends Thread{
+
+	Socket socket;
+	BufferedReader in = null;
+	
+	public Receiver (Socket socket) {
+		this.socket = socket;	
+		
+		try {
+			in = new BufferedReader(new 
+					InputStreamReader(socket.getInputStream()));
+		}
+		catch (Exception e) {
+			System.out.println("예외>Receiver>생성자:"+e);
+		}
+	}
+	
+	@Override
+	public void run() {
+		while (in != null) {
+			try {
+				System.out.println("Tread Receive : "+ in.readLine());
+			}
+			catch (SocketException e) {
+				System.out.println("SocketException");
+				break;
+			}
+			catch (Exception e) {
+				System.out.println("예외Receiver>run1:"+e);
+			}
+		}
+		
+		try {
+			in.close();
+		}
+		catch (Exception e) {
+			System.out.println("예외Receiver>run2:"+e);
+		}
+	}
+}
